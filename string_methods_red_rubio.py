4348e8908ad2929a6081355cf8c0619a7c98dc13
redstation <45374863+redstation@users.noreply.github.com>
2019-02-21 14:21:36 +0800

    Create string_methods_red_rubio.py

---
+++ string_methods_red_rubio.py
@@ -0,0 +1,23 @@
+print("#1Capitalized")
+word = "hello"
+print(word.capitalize())
+print()
+print("#Islower")
+word = "hi"
+print(word.islower())
+print()
+print("#Center")
+Joel = "Joel"
+print("Hello: ",red.center(20))
+print()
+print("#Isnumeric")
+number = "12345"
+print(number.isnumeric())
+print()
+print("#Casefold")
+word = "HELLO WORLD"
+print(word.casefold())
+print()
+print("#Isalpha")
+word = "hello world"
+print(word.isalpha())
