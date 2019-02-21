51cd6551a6658dadea79923af28bfcd6b4b55057
redstation <45374863+redstation@users.noreply.github.com>
2019-02-21 14:49:09 +0800

    Create prime_checker_red_rubio.py

---
+++ prime_checker_red_rubio.py
@@ -0,0 +1,12 @@
+number = int(input("Enter any number: "))
+
+if number >= 1:
+    for x in range(2, number):
+        if (number % x) == 0:
+            print(number, "is not a prime number")
+            break
+    else:
+        print(number, "is a prime number")
+
+else:
+    print(number, "is not a prime number")
