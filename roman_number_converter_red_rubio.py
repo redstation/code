5232f399c0d73bead506f648b807f723ce6ca3b0
redstation <45374863+redstation@users.noreply.github.com>
2019-02-21 14:47:50 +0800

    Create roman_number_converter_red_rubio.py

---
+++ roman_number_converter_red_rubio.py
@@ -0,0 +1,42 @@
+print("A.")
+print("Integer to Roman Numeral")
+def toRomanNum(x):
+    if x <= 200:
+        if x == 200:
+            print("CC",end="")
+        elif x < 200 and x >= 100:
+            print("C",end="")
+            y = x - 100
+            toRomanNum(y)
+        elif x == 99:
+            print("IC",end="")
+        elif x <= 98 and x >= 50:
+            print("L",end="")
+            z = x - 50
+            toRomanNum(z)
+        elif x == 49:
+            print("IL",end="")
+        elif x <= 48 and x >= 40:
+            print("XL",end="")
+            a = x - 40
+            toRomanNum(a)
+        elif x <= 39 and x >= 10:
+            print("X",end="")
+            a = x - 10
+            toRomanNum(a)
+        elif x <= 9 and x >= 5:
+            print("V",end="")
+            a = x - 5
+            toRomanNum(a)
+        elif x < 5 and x > 0:
+            print("I",end="")
+            a = x - 1
+            toRomanNum(a)
+        else:
+            print()
+toRomanNum(179)
+
+print()
+print("B.")
+print("Roman Numeral to Integer")
+def toInteger(x):
